f38b75d77e81aad86ec7cae07daca3edb5ad807c
Nagarian47 <onyx_nagarian47@hotmail.fr>
2015-12-19 18:01:39 +0100

    ajout du script sql pour créer la BDD

---
+++ AST-ynov/tokens.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]        INT           NOT NULL,
+    [Email]     NVARCHAR (50) NULL,
+    [CreatedAt] DATETIME      DEFAULT (getdate()) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_User_Token] FOREIGN KEY ([Id]) REFERENCES [dbo].[Tokens] ([Id])
+);
+GO
+CREATE TABLE [dbo].[Tokens] (
+    [Id]    INT           IDENTITY (1, 1) NOT NULL,
+    [Token] NVARCHAR (30) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+GO
